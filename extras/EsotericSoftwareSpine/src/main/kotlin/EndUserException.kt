a9f7633017a259e3e638ce654a6432abcafd6853
Daniel Wolf <dwolf@dannad.de>
2020-10-10 16:30:10 +0200

    Clean slate for Rhubarb 2

--- extras/EsotericSoftwareSpine/src/main/kotlin/EndUserException.kt
+++
@@ -1,4 +0,0 @@
-package com.rhubarb_lip_sync.rhubarb_for_spine
-
-// An exception with a human-readable message that can be shown to the end user
-class EndUserException(message: String): Exception(message)
--- extras/EsotericSoftwareSpine/src/main/kotlin/MouthCue.kt
+++
@@ -1,3 +0,0 @@
-package com.rhubarb_lip_sync.rhubarb_for_spine
-
-data class MouthCue(val time: Double, val mouthShape: MouthShape)
